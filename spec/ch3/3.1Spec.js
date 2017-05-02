dab78092d4f5e40684a482c579c50cde37390053
Tyler Weng <tylercweng@gmail.com>
2017-05-02 10:58:57 -0700

    Update stack functionality and bolster test cases

---
+++ spec/ch3/3.1Spec.js
@@ -0,0 +1,1 @@
+// No spec; word problem
---
+++ spec/ch3/3.2Spec.js
@@ -0,0 +1,31 @@
+describe("Stack with O(1) push, pop & min", function() {
+  const Stack = require("../../lib/ch3/3.2");
+
+  const stk1 = new Stack("A");
+  const stk2 = new Stack("A");
+  const stk3 = new Stack("C");
+  const sol1 = {
+    el: 8,
+    min: 5
+  };
+
+  beforeEach(function() {
+    stk1.push(10);
+    stk1.push(5);
+    stk1.push(8);
+    stk2.push(10);
+    stk2.push(5);
+    stk2.push(8);
+  });
+
+  it("pushes in elements properly", function() {
+    expect(Stack.stackComparator(stk1, stk2)).toBeTruthy();
+    expect(Stack.stackComparator(stk1, stk3)).toBeFalsy();
+  });
+
+  it("pops off the last el properly", function() {
+    const popped = stk1.pop();
+    expect(Stack.elComparator(popped, sol1)).toBeTruthy();
+  });
+
+});
