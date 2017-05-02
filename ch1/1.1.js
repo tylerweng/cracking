5594c9e0d44a382d0bcd19fb71950e1772a3f7c2
Tyler Weng <tylercweng@gmail.com>
2017-05-01 19:54:08 -0700

    Initialize repository and add first problem

---
+++ ch1/1.1.js
@@ -0,0 +1,20 @@
+// Implement an algorithm to determine if a string has all unique
+// characters. What if you can not use additional data structures?
+
+const allUniqueChars = str => {
+  const set = new Set();
+  for (let i = 0; i < str.length; i++) {
+    if (set.has(str[i])) return false;
+    set.add(str[i]);
+  }
+  return true;
+};
+
+const allUniqueChars2 = str => {
+  for (let i = 0; i < str.length - 1; i++) {
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) return false;
+    }
+  }
+  return true;
+};
