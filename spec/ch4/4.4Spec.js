c4136dd888cd1e9dbde3f40e004c3fafcef7ec21
Tyler Weng <tylercweng@gmail.com>
2017-05-10 13:52:27 -0700

    Implement 4.4 Create LinkedLists from BST

---
+++ spec/ch4/4.4Spec.js
@@ -0,0 +1,9 @@
+// const tree = new BST(5);
+// tree.left =  new BST(4);
+// tree.left.left = new BST(3);
+// tree.left.left.left = new BST(2);
+// tree.right =  new BST(6);
+
+// const buckets = createBuckets(tree);
+// const lists = createLists(buckets);
+// console.log(lists);
